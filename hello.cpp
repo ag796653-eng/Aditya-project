7a27dbf4d23e805a5d336866901f2468f3d3d676
aditya <ag796653@gmail.com>
2025-08-31 16:13:27 +0530

    first commit

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int age;
+    cin >> age;
+    if (age < 18)
+        cout << "Hello, Kid!" << endl;
+    else    
+    cout << "not a kid" << endl;
+//  cout << "Hello, World!" << endl;
+ return 0;
+}
